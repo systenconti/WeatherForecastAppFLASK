9f04936dfbd25286d772193c83b4a82c3049130b
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-04-27 16:53:30 +0200

    Create plotly graph if user chooses temperature option

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from backend import get_data
+import plotly_express as px
 
 app = Flask(__name__)
 
@@ -10,13 +11,21 @@ def weather():
         city = request.form["city"]
         day = request.form["day"]
         forecasttype = request.form["forecasttype"]
-        print(city, day, forecasttype)
 
         filtered_data = get_data(place=city, forecast_days=int(day))
         dates = [item["dt_txt"] for item in filtered_data]
         temperatures = [item["main"]["temp"] for item in filtered_data]
         sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
-        print(dates, temperatures, sky_conditions)
+
+        if forecasttype == "temperature":
+            figure = px.line(
+                x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}
+            )
+            graph_json = figure.to_json()
+            return render_template("weather.html", graph_json=graph_json)
+        
+        elif forecasttype == "sky":
+            
     return render_template("weather.html")
 
 
