d2d3604572ac2062b95186799e606f3936aa3499
Bartosz Sowa <bartoszsowa.bsi@gmail.com>
2023-04-27 21:03:01 +0200

    Display sky conditions when user chooses "sky" option

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import plotly_express as px
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def weather():
     if request.method == "POST":
@@ -15,17 +14,29 @@ def weather():
         filtered_data = get_data(place=city, forecast_days=int(day))
         dates = [item["dt_txt"] for item in filtered_data]
         temperatures = [item["main"]["temp"] for item in filtered_data]
-        sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
 
         if forecasttype == "temperature":
             figure = px.line(
                 x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"}
             )
             graph_json = figure.to_json()
-            return render_template("weather.html", graph_json=graph_json)
-        
+            return render_template(
+                "weather.html", forecasttype=forecasttype, graph_json=graph_json
+            )
+
         elif forecasttype == "sky":
-            
+            sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
+            forecast_data = {}
+            for date, sky_condition in zip(dates, sky_conditions):
+                if date not in forecast_data:
+                    forecast_data[date] = ""
+                forecast_data[date] = sky_condition.lower()
+            print(forecast_data.items())
+            return render_template(
+                "weather.html",
+                forecasttype=forecasttype,
+                forecast_data=forecast_data
+            )
     return render_template("weather.html")
 
 
